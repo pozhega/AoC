0072d7a83389c72e8a19f6da75f3572b7403daf6
Marko Požega <mpozega94@gmail.com>
2020-12-01 12:24:57 +0100

    Cast inputs to integer while loading puzzle in d1

--- 2020/d1.py
+++ 2020/d1.py
@@ -2,25 +2,25 @@
 
 
 def part1(data):
-    for i in range(0, len(DATA) - 1):
-        for j in range(i + 1, len(DATA) - 1):
-            e1, e2 = int(DATA[i]), int(DATA[j])
+    for i in range(0, len(data) - 1):
+        for j in range(i + 1, len(data) - 1):
+            e1, e2 = data[i], data[j]
             if e1 + e2 == 2020:
                 return e1 * e2
 
 
 def part2(data):
-    for i in range(0, len(DATA) - 1):
-        for j in range(i + 1, len(DATA) - 1):
-            for z in range(j + 1, len(DATA) - 1):
-                e1, e2, e3 = int(DATA[i]), int(DATA[j]), int(DATA[z])
+    for i in range(0, len(data) - 1):
+        for j in range(i + 1, len(data) - 1):
+            for z in range(j + 1, len(data) - 1):
+                e1, e2, e3 = data[i], data[j], data[z]
                 if e1 + e2 + e3 == 2020:
                     return e1 * e2 * e3
 
 
 if __name__ == "__main__":
-    test = ["1721", "979", "366", "299", "675", "1456"]
-    puzzle = [line.strip() for line in open("inputs/d1.txt", "r")]
+    test = [1721, 979, 366, 299, 675, 1456]
+    puzzle = [int(line.strip()) for line in open("inputs/d1.txt", "r")]
 
     print(part1(test))
     print(part1(puzzle))
