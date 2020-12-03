b8ca95d594dd1ad1140a56d28411993ca10d638b
Marko Požega <mpozega94@gmail.com>
2020-12-03 09:39:04 +0100

    Add 2020/d3 solution

--- 2020/d2.py
+++ 2020/d2.py
@@ -9,19 +9,19 @@ import re
 
 
 def part1(count, policy):
-    low, high, letter, pwd = re.match(LINE_PATTERN, policy).groups()
+    low, high, letter, pwd = re.match(INPUT_PATTERN, policy).groups()
     return count + (int(low) <= pwd.count(letter) <= int(high))
 
 
 def part2(count, policy):
-    pos1, pos2, letter, pwd = re.match(LINE_PATTERN, policy).groups()
+    pos1, pos2, letter, pwd = re.match(INPUT_PATTERN, policy).groups()
     return count + ((pwd[int(pos1)-1] + pwd[int(pos2)-1]).count(letter) == 1)
 
 
 if __name__ == "__main__":
     TEST = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
     PUZZLE = [line.strip() for line in open("inputs/d2.txt", "r")]
-    LINE_PATTERN = re.compile(r"(\d+)-(\d+) (\w): (\w*)")
+    INPUT_PATTERN = re.compile(r"(\d+)-(\d+) (\w): (\w*)")
 
     print(reduce(part1, TEST, 0))
     print(reduce(part1, PUZZLE, 0))
---
+++ 2020/d3.py
@@ -0,0 +1,35 @@
+""" https://adventofcode.com/2020/day/3 """
+
+from functools import reduce
+
+
+def part1(map, step):
+    """ O(n) solution """
+
+    x, y = step[0], step[1]
+    height, width = len(map), len(map[0])
+    trees = 0
+    while y < height:
+        trees += (map[y][x] == "#")
+        x = (x + step[0]) % width
+        y += step[1]
+
+    return trees
+
+
+def part2(map, steps):
+    """ O(n) solution """
+
+    return reduce(lambda x, step: x * part1(map, step), steps, 1)
+
+
+if __name__ == "__main__":
+    TEST = [line.strip() for line in open("inputs/d3_test.txt", "r")]
+    PUZZLE = [line.strip() for line in open("inputs/d3.txt", "r")]
+    PART_1_STEP = (3, 1)
+    PART_2_STEPS = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
+    print(part1(TEST, PART_1_STEP))
+    print(part1(PUZZLE, PART_1_STEP))
+    print(part2(TEST, PART_2_STEPS))
+    print(part2(PUZZLE, PART_2_STEPS))
