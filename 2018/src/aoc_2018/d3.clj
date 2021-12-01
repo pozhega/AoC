7c8a7e8cbed5bb93bf03ee93a2b48b19dd0fc6c9
Marko Požega <mpozega94@gmail.com>
2021-12-01 01:01:48 +0100

    Solve 2018/d3

---
+++ 2018/src/aoc_2018/d3.clj
@@ -0,0 +1,73 @@
+(ns aoc-2018.d3 (:require
+                 [clojure.string :as str]
+                 [clojure.set]))
+
+(defn- parse-input [path]
+  (->> path
+       (slurp)
+       (str/split-lines)
+       (map (fn [line]
+              (->> line
+                   (re-matcher #"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)")
+                   (re-find)
+                   (rest)
+                   (map #(Integer/parseInt %))
+                   (vec))))))
+
+
+(defn- generate-fabric []
+  (vec (repeat 1000 (vec (repeat 1000 nil)))))
+
+(defn- fields-from-claim [[_ left top width height]]
+  (for [row (range top (+ top height))
+        col (range left (+ left width))]
+    [row col]))
+
+(defn- update-fabric-fields [fabric fields id]
+  (reduce (fn [fabric field] (update-in fabric field #(if (nil? %) id false)))
+          fabric
+          fields))
+
+(defn- apply-claims [fabric claims]
+  (reduce #(update-fabric-fields %1 (fields-from-claim %2) (first %2))
+          fabric
+          claims))
+
+(defn- count-overlaps [fabric]
+  (->> fabric
+       (filter false?)
+       (count)))
+
+(defn- no-overlap? [fabric-freq [id _ _ width height]]
+  (= (* width height) (fabric-freq id)))
+
+(defn part-1
+  "How many square inches of fabric are within two or more claims?"
+  [claims]
+  (-> (generate-fabric)
+      (apply-claims claims)
+      (flatten)
+      (count-overlaps)))
+
+(defn part-2
+  "What is the ID of the only claim that doesn't overlap?"
+  [claims]
+  (let [freqs (-> (generate-fabric)
+                  (apply-claims claims)
+                  (flatten)
+                  (frequencies))]
+    (->> claims
+         (filter (partial no-overlap? freqs))
+         (first)
+         (first))))
+
+;; Puzzle: https://adventofcode.com/2018/day/3
+
+(def test-input (parse-input "resources/tests/d3.txt"))
+(def input (parse-input "resources/puzzles/d3.txt"))
+
+(= (part-1 test-input) 4)
+(part-1 input)
+
+(= (part-2 test-input) 3)
+(part-2 test-input)
