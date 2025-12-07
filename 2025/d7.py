13298c80fe27e64749d72eb80260cf7e8e507cb5
Marko Požega <mpozega94@gmail.com>
2025-12-07 12:25:37 +0100

    Cleaner day 07

--- 2025/d7.py
+++ 2025/d7.py
@@ -11,59 +11,42 @@ def parse_input(filename: str):
     return map
 
 
-def part1(data: Map):
+def is_int(val: str | int) -> bool:
+    return isinstance(val, int)
+
+
+def solve(data: Map):
     splits = 0; map = deepcopy(data)
-    map[1][len(map[1]) // 2] = "|"
+    map[1][len(map[1]) // 2] = 1
     
-    for row_i in range(2, len(map), 2):
+    for row_i in range(2, len(map)):
         for col_i, val in enumerate(map[row_i]):
-            if val == "." and map[row_i - 1][col_i] == "|":
-                map[row_i][col_i] = "|"
-                map[row_i + 1][col_i] = "|"
-            elif val == "^" and map[row_i - 1][col_i] == "|":
-                map[row_i][col_i - 1] = "|"
-                map[row_i + 1][col_i - 1] = "|"
-                map[row_i][col_i + 1] = "|"
-                map[row_i + 1][col_i + 1] = "|"
-                splits += 1
-
-    return splits
-
-
-def part2(data: Map):
-    map = deepcopy(data)
-    map[1][len(map[1]) // 2] = "1"
-    
-    for row_i in range(2, len(map), 2):
-        for col_i, val in enumerate(map[row_i]):
-            active_parent = map[row_i - 1][col_i].isnumeric()
             parent_val = map[row_i - 1][col_i]
-            if val == "." and active_parent:
+            if not is_int(parent_val): continue
+
+            if val == ".":
                 map[row_i][col_i] = parent_val
-                map[row_i + 1][col_i] = parent_val
-            elif val == "^" and active_parent:
-                active_left = map[row_i][col_i - 1].isnumeric()
+            elif val == "^":
                 left_val = map[row_i][col_i - 1]
-                new_left_val = str(int(left_val) + int(parent_val)) if active_left else parent_val
-                map[row_i][col_i - 1] = new_left_val
-                map[row_i + 1][col_i - 1] = new_left_val
+                map[row_i][col_i - 1] = left_val + parent_val if is_int(left_val) else parent_val
                 map[row_i][col_i + 1] = parent_val
-                map[row_i + 1][col_i + 1] = parent_val
-            elif active_parent:
-                map[row_i][col_i] = str(int(val) + int(parent_val))
-                map[row_i + 1][col_i] = str(int(val) + int(parent_val))
+                splits += 1
+            else:
+                map[row_i][col_i] = val + parent_val
     
-    return sum([int(val) for val in map[-1] if val.isnumeric()])
+    timelines = sum([val for val in map[-1] if is_int(val)])
+    
+    return (splits, timelines)
 
 
 if __name__ == "__main__":
     TEST = parse_input("tests/d7.txt")
     PUZZLE = parse_input("puzzles/d7.txt")
 
-    # print ("Part 1(TEST):", part1(TEST))
-    # assert part1(TEST) == 21
-    print(f"Part 1: {part1(PUZZLE)}")
+    # print ("Part 1(TEST):", solve(TEST)[0])
+    # assert solve(TEST)[0] == 21
+    print(f"Part 1: {solve(PUZZLE)[0]}")
 
-    # print ("Part 2(TEST):", part2(TEST))
-    # assert part2(TEST) == 40
-    print(f"Part 2: {part2(PUZZLE)}")
+    # print ("Part 2(TEST):", solve(TEST)[1])
+    # assert solve(TEST)[1] == 40
+    print(f"Part 2: {solve(PUZZLE)[1]}")
